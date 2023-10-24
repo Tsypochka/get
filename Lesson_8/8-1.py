74cfdc640b245b5df344d3609dfcfd7e5d8402a5
Даниил Цыпочка <tsypochka.dg@phystech.edu>
2023-10-24 11:42:57 +0300

    Added Lesson 8

---
+++ Lesson_8/8-1.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt
+with open('settings.txt', 'r') as set:
+    settings = [float(i) for i in set.read().split("\n")]
+data = np.loadtxt("data.txt", dtype=int)
+
+data = data * settings[1] #acp data to volts
+x = np.arange(1, len(data) + 1, 1, dtype=int)
+x = x / settings[0] #number to seconds
+
+fig, ax = plt.subplots(figsize=(16, 10), dpi=400)
+
+ax.set_xlabel('Time, s')
+ax.set_ylabel('Voltage, V')
+plt.title('Capacitor Charging and Discharging in RC circuit')
+
+ax.plot(x, data, 'o', label='V(t)')
+plt.legend()
+
+plt.minorticks_on() #turn on minorsticks 
+
+plt.grid(which='major')
+plt.grid(which='minor', linestyle=':')
+#add Charging/Discharging time
+plt.text(6, 1, 'Charging time = 8.55 c')
+plt.text(6, 0.9, 'Discharging time = 5.95 c')
+
+fig.savefig("data_grafic.svg")
+plt.show()
